edc9497b3921e8b3a06aa93a37572339323bfa3b
Odd Karsten Svartaas <oksvart@gmail.com>
2023-01-17 19:48:21 +0100

    Prettify files

--- babel.config.js
+++ babel.config.js
@@ -2,23 +2,23 @@
 // https://github.com/kentcdodds/babel-plugin-macros/issues/131
 // 17/01/20: does not fix the issue
 module.exports = {
-	presets: ['@babel/preset-env'],
-	plugins: ['@babel/plugin-proposal-optional-chaining'],
-	env: {
-		test: {
-			plugins: [
-				'@babel/plugin-transform-runtime',
-				'@babel/plugin-proposal-class-properties',
-			],
-			presets: [
-				[
-					'@babel/env',
-					{
-						useBuiltIns: 'usage',
-						corejs: 3,
-					},
-				],
-			],
-		},
-	},
+  presets: ['@babel/preset-env'],
+  plugins: ['@babel/plugin-proposal-optional-chaining'],
+  env: {
+    test: {
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-proposal-class-properties',
+      ],
+      presets: [
+        [
+          '@babel/env',
+          {
+            useBuiltIns: 'usage',
+            corejs: 3,
+          },
+        ],
+      ],
+    },
+  },
 };
--- config-overrides.js
+++ config-overrides.js
@@ -3,29 +3,29 @@
  */
 
 module.exports = {
-	webpack(config, env) {
-		config.optimization.runtimeChunk = false;
-		config.optimization.splitChunks = {
-			cacheGroups: {
-				default: false,
-			},
-		};
-		return config;
-	},
-	jest(config) {
-		config.transform = {
-			'^.+\\.[t|j]sx?$': 'babel-jest',
-			'.+\\\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-				'jest-transform-stub',
-		};
-		config.transformIgnorePatterns = [
-			'node_modules/core-js/',
-			'node_modules/(?!(@my_departures))/',
-		];
-		config.moduleNameMapper = {
-			'^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-				'jest-transform-stub',
-		};
-		return config;
-	},
+  webpack(config, env) {
+    config.optimization.runtimeChunk = false;
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        default: false,
+      },
+    };
+    return config;
+  },
+  jest(config) {
+    config.transform = {
+      '^.+\\.[t|j]sx?$': 'babel-jest',
+      '.+\\\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+        'jest-transform-stub',
+    };
+    config.transformIgnorePatterns = [
+      'node_modules/core-js/',
+      'node_modules/(?!(@my_departures))/',
+    ];
+    config.moduleNameMapper = {
+      '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+        'jest-transform-stub',
+    };
+    return config;
+  },
 };
